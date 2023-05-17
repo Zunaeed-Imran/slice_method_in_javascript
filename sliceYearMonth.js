a961a8f83f023705fe1cfbde290b3d7f3bed8828
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-17 11:05:28 +0600

    slice year month for one input(prompt).Number of d
    slice year month for one input(prompt). Number of days in a given month of a given year

---
+++ sliceYearMonth.js
@@ -0,0 +1,21 @@
+let year = prompt("Enter a year: ");
+let month = year.slice(05, 07);
+let day;
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+  if (month == "02") {
+    day = 29;
+  } else if (month == "09" || month == "04" || month == "06" || month == "11") {
+    day = 30;
+  } else {
+    day = 31;
+  }
+} else {
+  if (month == "02") {
+    day = 28;
+  } else if (month == "09" || month == "04" || month == "06" || month == "11") {
+    day = 30;
+  } else {
+    day = 31;
+  }
+}
+document.write(day);
